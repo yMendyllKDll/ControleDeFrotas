e1206749ed01b412ce8158ebdafcff67a2639c0a
Anderson <and_mendescosta@hotmail.com>
2022-03-07 17:24:50 -0300

    Correção

--- Menu/View/Home.Designer.cs
+++ Menu/View/Home.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            BunifuAnimatorNS.Animation animation1 = new BunifuAnimatorNS.Animation();
+            BunifuAnimatorNS.Animation animation2 = new BunifuAnimatorNS.Animation();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
             this.MenuLateral = new System.Windows.Forms.Panel();
             this.btnRelatorios = new Bunifu.Framework.UI.BunifuFlatButton();
@@ -190,6 +190,7 @@
             this.btnAvisos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.btnAvisos.Textcolor = System.Drawing.Color.Black;
             this.btnAvisos.TextFont = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAvisos.Visible = false;
             this.btnAvisos.Click += new System.EventHandler(this.BtnAvisos_Click);
             // 
             // btnVeiculosDisponiveis
@@ -227,6 +228,7 @@
             this.btnVeiculosDisponiveis.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.btnVeiculosDisponiveis.Textcolor = System.Drawing.Color.Black;
             this.btnVeiculosDisponiveis.TextFont = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVeiculosDisponiveis.Visible = false;
             this.btnVeiculosDisponiveis.Click += new System.EventHandler(this.BtnVeiculosDisponiveis_Click);
             // 
             // panelFinanceiroSubMenu
@@ -883,22 +885,22 @@
             // 
             this.tshow.AnimationType = BunifuAnimatorNS.AnimationType.HorizSlide;
             this.tshow.Cursor = null;
-            animation1.AnimateOnlyDifferences = true;
-            animation1.BlindCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.BlindCoeff")));
-            animation1.LeafCoeff = 0F;
-            animation1.MaxTime = 1F;
-            animation1.MinTime = 0F;
-            animation1.MosaicCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.MosaicCoeff")));
-            animation1.MosaicShift = ((System.Drawing.PointF)(resources.GetObject("animation1.MosaicShift")));
-            animation1.MosaicSize = 0;
-            animation1.Padding = new System.Windows.Forms.Padding(0);
-            animation1.RotateCoeff = 0F;
-            animation1.RotateLimit = 0F;
-            animation1.ScaleCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.ScaleCoeff")));
-            animation1.SlideCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.SlideCoeff")));
-            animation1.TimeCoeff = 0F;
-            animation1.TransparencyCoeff = 0F;
-            this.tshow.DefaultAnimation = animation1;
+            animation2.AnimateOnlyDifferences = true;
+            animation2.BlindCoeff = ((System.Drawing.PointF)(resources.GetObject("animation2.BlindCoeff")));
+            animation2.LeafCoeff = 0F;
+            animation2.MaxTime = 1F;
+            animation2.MinTime = 0F;
+            animation2.MosaicCoeff = ((System.Drawing.PointF)(resources.GetObject("animation2.MosaicCoeff")));
+            animation2.MosaicShift = ((System.Drawing.PointF)(resources.GetObject("animation2.MosaicShift")));
+            animation2.MosaicSize = 0;
+            animation2.Padding = new System.Windows.Forms.Padding(0);
+            animation2.RotateCoeff = 0F;
+            animation2.RotateLimit = 0F;
+            animation2.ScaleCoeff = ((System.Drawing.PointF)(resources.GetObject("animation2.ScaleCoeff")));
+            animation2.SlideCoeff = ((System.Drawing.PointF)(resources.GetObject("animation2.SlideCoeff")));
+            animation2.TimeCoeff = 0F;
+            animation2.TransparencyCoeff = 0F;
+            this.tshow.DefaultAnimation = animation2;
             // 
             // panelSuperior
             // 
